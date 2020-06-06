40c6789c91a294f624f8d206905245e9595de525
Rodrigo Aguilar <rod.a.nato@gmail.com>
2020-06-05 20:12:34 -0500

    feat(utils): Global styles feat(utils): media-queries functions style(reset): Reset css feat(main): Base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // eslint-disable-line
+import { ThemeProvider } from 'emotion-theming';
+
+/** @jsx jsx */
+import { Global, jsx, css } from '@emotion/core';
+
+import MainOrganism from './main/main.organism';
+import HeaderOrganism from './main/header/header.organism';
+import SliderOrganism from './main/slider/slider.organism';
+import FooterOrganism from './main/footer/footer.organism';
+import NavigationOrganism from './main/navigation/navigation.organism';
+
+import reset from './reset';
+import globalClasses from './utils/global-styles';
+
+const theme1 = {// eslint-disable-line
+  bg: 'hotpink'
+}
+
+const theme2 = {
+  bg: '#5742cc'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme2}>
+      <Global styles={css`
+        ${reset}
+        ${globalClasses}
+      `} />
+
+      <MainOrganism
+        header={<HeaderOrganism/>}
+        slider={<SliderOrganism/>}
+        footer={<FooterOrganism/>}
+        navigation={<NavigationOrganism/>}
+      >
+      </MainOrganism>
+    </ThemeProvider>
   );
 }
 
